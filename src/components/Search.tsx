6ec0ea92140d0a7fc1630886738da811a0ab13a8
Fabio Fernandes <fabiofernandes2002@gmail.com>
2026-04-08 20:15:56 -0300

    feat: add autocomplete to the search bar

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -2,7 +2,8 @@ import { MagnifyingGlassIcon } from "@phosphor-icons/react";
 import React, { useState, useEffect } from "react";
 
 const Search = () => {
-  interface geocodingData {
+  //interface para tipar os dados necessários para serem usados na aplicação que a api irá retornar quando fizer a chamada.
+  interface City {
     id: number;
     name: string;
     latitude: number;
@@ -11,35 +12,47 @@ const Search = () => {
     admin1?: string;
   }
 
-  const [city, setCity] = useState<string>("");
-  const [suggestions, setSuggestions] = useState([]);
+  //interface para separar os obejcts das cidades que vem dentro de um array result.
+  interface GeocodingResponse {
+    results?: City[] | any;
+  }
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     if (city.length > 2) {
-  //       // fetchCity(city);
-  //       console.log("Váriavel alterada");
-  //     } else {
-  //       setSuggestions([]);
-  //     }
-  //   }, 1000);
+  const [city, setCity] = useState<string>(""); // Variável que vai armazenar o nome da cidade que o usuário digitar na searchBar.
+  const [suggestions, setSuggestions] = useState<any>([]); // Variável que vai armazenar os dados retornados pela api.
+  const [showDroppdown, setShowDroppdown] = useState<boolean>(false);
 
-  //   return () => {
-  //     clearTimeout(timer);
-  //   };
-  // }, [city]); // O "[]" garante que a chamada para a Api só irá acontecer uma vez quando o programa se inicia
+  // useEffect permite que toda a ação que estiver dentro dele seja executada
+  // apenas com uma condição ou mudança definida.
+  useEffect(() => {
+    // Cria um timer que vai fazer a chamada da api somente depois que o tempo definido terminar.
+    const timer = setTimeout(() => {
+      if (city.length > 2) {
+        // fetchCity(city);
+        fetchCity(city);
+        console.log(suggestions);
+      } else {
+        setSuggestions([]);
+      }
+    }, 1000);
 
-  // const fetchCity = async (city: String): Promise<void> => {
-  //   try {
-  //     const response = await fetch(
-  //       `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=5&language=pt`,
-  //     );
-  //     const data = await response.json();
-  //     console.log("Teste de Conexão", data);
-  //   } catch (err) {
-  //     console.error("Conexão não estabelecida", err);
-  //   }
-  // };
+    return () => {
+      // Reinicia o timer para ser iniciado novamente assim que o usuário digitar de novo
+      clearTimeout(timer);
+    };
+  }, [city]); // O "[city]" garante que a chamada para a Api só irá acontecer quando a váriavel city mudar
+
+  const fetchCity = async (city: String): Promise<City[] | any> => {
+    try {
+      const response = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=5&language=pt`,
+      );
+      const data: GeocodingResponse = await response.json();
+      setSuggestions(data.results || []);
+      setShowDroppdown(true);
+    } catch (err) {
+      console.error("Conexão não estabelecida", err);
+    }
+  };
 
   return (
     <>
@@ -58,27 +71,22 @@ const Search = () => {
           />
         </div>
       </header>
-
-      {/* Container dropdown para as sugestões de autocomplete das cidades */}
-      <div className="flex justify-center items-center">
-        <div className="w-4/5 lg:w-4xl md:text-xl font-bold px-2 py-2 flex flex-col gap-2 rounded-2xl shadow-xl backdrop-blur-md bg-white/20 border-white/30 text-white">
-          <div className="hover:bg-white/30 cursor-pointer p-1 pl-2 rounded-xl">
-            São Paulo, Brasil
-          </div>
-          <div className="hover:bg-white/30 cursor-pointer p-1 pl-2 rounded-xl">
-            São Paulo, Brasil
-          </div>
-          <div className="hover:bg-white/30 cursor-pointer p-1 pl-2 rounded-xl">
-            São Paulo, Brasil
-          </div>
-          <div className="hover:bg-white/30 cursor-pointer p-1 pl-2 rounded-xl">
-            São Paulo, Brasil
-          </div>
-          <div className="hover:bg-white/30 cursor-pointer p-1 pl-2 rounded-xl">
-            São Paulo, Brasil
+      {showDroppdown && suggestions.length > 0 && (
+        <div className="flex justify-center items-center">
+          <div className="w-4/5 lg:w-4xl md:text-xl font-bold px-2 py-2 flex flex-col gap-2 rounded-2xl shadow-xl backdrop-blur-md bg-white/20 border-white/30 text-white">
+            <ul>
+              {suggestions.map((item: any) => (
+                <li
+                  className="hover:bg-white/30 cursor-pointer p-1 pl-2 rounded-xl"
+                  key={item.id}
+                >
+                  {item.name}, {item.admin1}, {item.country}
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
